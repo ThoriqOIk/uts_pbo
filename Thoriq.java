4a2e16af22bb6548fc1b9004f173acbc2756d0d0
ThoriqOik <95134513+ThoriqOIk@users.noreply.github.com>
2023-05-23 18:24:21 +0700

    Delete Thoriq.java

--- Thoriq.java
+++
@@ -1,9 +0,0 @@
-package coba ;
-
-class Thoriq extends mahasiswa{
-    public void tampil(){
-    Thoriq oik = new Thoriq();
-    oik.inputdata("Muhammad THoriq", 1217050104, "lakilaki");
-    oik.tampilkandata();
-}
-}
