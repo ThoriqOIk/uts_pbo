aad971242290623fb0fad0fd33c454c7b19e55a9
ThoriqOik <95134513+ThoriqOIk@users.noreply.github.com>
2023-05-23 18:26:21 +0700

    Add files via upload

---
+++ Ejot.java
@@ -0,0 +1,9 @@
+package coba ;
+
+class Ejot extends mahasiswa{
+    public void tampil(){
+    Ejot oik = new Ejot();
+    oik.inputdata("Reza Fahlevi", 1217050120, "lakilaki");
+    oik.tampilkandata();
+}
+}
---
+++ uts.java
@@ -0,0 +1,12 @@
+package coba ;
+
+class uts{
+    public static void main(String[]args){
+        Thoriq iq = new Thoriq();
+        System.out.println("Thoriq");
+        iq.tampil();
+        System.out.println("Ejot");
+        Ejot jot = new Ejot();
+        jot.tampil();
+    }
+}
